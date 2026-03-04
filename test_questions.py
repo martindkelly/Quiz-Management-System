bc06da621c2a0a6dead56ffccc1aca9f401daadf
martindkelly <149399714+martindkelly@users.noreply.github.com>
2026-03-04 00:14:42 +0000

    Add failing test for TrueFalseQuestion

--- test_questions.py
+++ test_questions.py
@@ -1,7 +1,12 @@
-from questions import MultipleChoiceQuestion
+from questions import MultipleChoiceQuestion, TrueFalseQuestion
 
-def test_correct_answer_returns_true():
+def test_multiple_choice_correct_answer_returns_true():
     q = MultipleChoiceQuestion(question="What colour is the sky",
                                options=["yellow", "red", "green", "blue"],
                                correct_answer="blue")
     assert q.check_answer("blue") == True
+
+def test_true_false_correct_answer_returns_true():
+    q = TrueFalseQuestion(question="Are there 7 days in a week?",
+                          correct_answer=True)
+    assert q.check_answer("True") == True
