42d05203e35741aaba21128c340793e194071bf7
ace-of-spade-s <gingerwellfan@gmail.com>
2026-03-07 18:32:06 +0000

    mades the scores.py file

---
+++ scores.py
@@ -0,0 +1,21 @@
+
+class Scores:
+
+    def __init__(self):
+        self.correct = 0
+        self.total = 0
+    
+    def inc_score(self):
+        self.correct += 1
+        self.total += 1
+
+    def inc_wrong(self):
+        self.total += 1
+
+    def get_correct(self):
+        return self.correct
+    
+    def get_total(self):
+        return self.total
+    
+    
